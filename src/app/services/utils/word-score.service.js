afd322128d7914782585648db6d0258fd65f8a53
Dario Di Fazio <difada1@hotmail.com>
2016-03-02 22:14:41 +0100

    add service to calculate the score

---
+++ src/app/services/utils/word-score.service.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .factory('wordScoreService', wordScoreService);
+
+  /** @ngInject */
+  function wordScoreService() {
+
+    var service = {
+      calculateScore: calculateScore,
+      calculateMaxScore: calculateMaxScore
+    };
+
+    return service;
+
+
+    /**
+     * Calculates the score for the given word and number of deletes clicked
+     * @param word
+     * @param numberOfDeletes
+     * @returns (*)
+     */
+    function calculateScore(word, numberOfDeletes) {
+
+      var _numberOfDeletes = numberOfDeletes || 0;
+
+      return (calculateMaxScore(word) - _numberOfDeletes);
+    }
+
+    /**
+     * Calculates the max_score for the given word
+     * @param word
+     * @returns (*)
+     */
+    function calculateMaxScore(word) {
+
+      if (!word) {
+        throw Error('No Word provided');
+      }
+
+      return Math.floor(Math.pow(1.95, (word.length / 3)));
+    }
+
+  }
+})();
