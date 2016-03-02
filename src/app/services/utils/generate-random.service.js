d4e0ba4efdbdbabdd6d6d10b2707564f5fc10f5b
Dario Di Fazio <difada1@hotmail.com>
2016-03-02 22:52:39 +0100

    add random number generator

---
+++ src/app/services/utils/generate-random.service.js
@@ -0,0 +1,46 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .factory('generateRandomService', generateRandomService);
+
+  /** @ngInject */
+  function generateRandomService() {
+
+    var service = {
+      generateRandomArray: generateRandomArray,
+      generateRandomNumber: generateRandomNumber
+    };
+
+    return service;
+
+
+    /**
+     * Creates an array with random numbers
+     * @param length
+     * @returns {Array}
+       */
+    function generateRandomArray(length) {
+
+      var randomArray = [];
+
+      for (var i = 0; i <= length; i++) {
+          randomArray.push(generateRandomNumber());
+      }
+
+      return randomArray;
+    }
+
+
+    /**
+     * Creates a random number
+     * @returns {*|number}
+       */
+    function generateRandomNumber() {
+
+      return __.random(100);
+    }
+
+  }
+})();
